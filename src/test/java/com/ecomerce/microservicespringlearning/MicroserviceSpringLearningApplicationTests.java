adccf47a20f0f5f681a356ed9dea559dbd2d0546
phamnhatkhanh <khanhhcm4@gmail.com>
2023-09-22 13:04:16 +0700

    Services: build product, order, inventory

---
+++ src/test/java/com/ecomerce/microservicespringlearning/MicroserviceSpringLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomerce.microservicespringlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceSpringLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
